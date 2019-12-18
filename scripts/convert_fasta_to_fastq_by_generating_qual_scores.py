95ebb28f8ce57efa695922f8dbece8205e57bb25
Marc Galland <m.galland@uva.nl>
2019-12-18 18:17:50 +0100

    fix small mistake in read simulation

--- scripts/convert_fasta_to_fastq_by_generating_qual_scores.py
+++ scripts/convert_fasta_to_fastq_by_generating_qual_scores.py
@@ -1,28 +1,40 @@
 """
 Convert FASTA to FASTQ file with a static
 Usage:
-$ ./fasta_to_fastq NAME.fasta NAME.fastq
+$ ./fasta_to_fastq -i NAME.fasta -o NAME.fastq -m 30
 
 Source: https://www.biostars.org/p/99886/
 
 For more on Phred scores see https://www.drive5.com/usearch/manual/quality_score.html
 """
 
-import sys, os
 import argparse
 from Bio import SeqIO
+import random
+
+# parse the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--fasta_file", help = "The path to the input fasta file to convert")
+parser.add_argument("-o","--fastq_file", help = "The path to the output fastq file file to write")
+parser.add_argument("-m", "--min_quality", type = int, help = "The minimal Phred33 score quality value", default = 30)
+parser.add_argument("-M", "--max_quality", type=int, help = "The maximal Phred33 score quality value ", default = 40)
+args = parser.parse_args()
+
 
 # Get inputs
-fa_path = sys.argv[1]
-fq_path = sys.argv[2]
+fa_path = args.fasta_file
+fq_path = args.fastq_file
 
 # min and max values for Phred quality scores (Phred33)
-min_qual = sys.argv[3]
-max_qual = sys.argv[4]
+min_qual = args.min_quality
+max_qual = args.max_quality
 
 # make fastq
 with open(fa_path, "r") as fasta, open(fq_path, "w") as fastq:
     for record in SeqIO.parse(fasta, "fasta"):
-    	quality = random.randrange(min_qual, max_qual, step = 1)
-        record.letter_annotations["phred_quality"] = [quality] * len(record)
+        phred_score_qualities = random.choices(
+        	list(range(min_qual, max_qual + 1, 1)),
+            k = len(record)
+            )
+        record.letter_annotations["phred_quality"] = phred_score_qualities
         SeqIO.write(record, fastq, "fastq")
